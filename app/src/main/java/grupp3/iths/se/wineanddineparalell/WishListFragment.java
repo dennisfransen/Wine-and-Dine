7e83eb5519f474bbbe008ed41ad5e35826c50651
isabelpebaque <isabel.pebaque@gmail.com>
2018-11-20 12:15:39 +0100

    Added some javadoc to methods

--- app/src/main/java/grupp3/iths/se/wineanddineparalell/WishListFragment.java
+++ app/src/main/java/grupp3/iths/se/wineanddineparalell/WishListFragment.java
@@ -4,7 +4,6 @@ package grupp3.iths.se.wineanddineparalell;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
