1adfe1f12cc32f84c33e2ad1526faad10bf84ee4
Essi3 <esroozen@live.se>
2018-11-21 08:46:31 +0100

    Added Layout.

--- app/src/main/java/grupp3/iths/se/wineanddineparalell/CapturePictureActivity.java
+++ app/src/main/java/grupp3/iths/se/wineanddineparalell/CapturePictureActivity.java
@@ -2,8 +2,11 @@ package grupp3.iths.se.wineanddineparalell;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CapturePictureActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
