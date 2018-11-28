27891ca6b8bae17b54236f7d4f313acf3e8a8de2
Essi3 <esroozen@live.se>
2018-11-28 15:00:20 +0100

    database MakeReviewFragment

--- app/src/main/java/grupp3/iths/se/wineanddineparalell/MakeReviewFragment.java
+++ app/src/main/java/grupp3/iths/se/wineanddineparalell/MakeReviewFragment.java
@@ -2,25 +2,44 @@ package grupp3.iths.se.wineanddineparalell;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MakeReviewFragment extends Fragment {
-    private FirebaseFirestore mFireStore = FirebaseFirestore.getInstance();
-    private CollectionReference restaurantRef = mFireStore.collection("reviews");
+
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    private RatingBar mStar;
+    private EditText mReview;
+    private Button mSendReviewBtn;
+
     private ReviewAdapter reviewAdapter;
 
 
@@ -29,11 +48,48 @@ public class MakeReviewFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_make_review, container, false);
 
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        final DocumentReference restaurantDocumentRef = firebaseFirestore.collection("restaurant")
+                .document("Glenns");
+
+        // Bound Variables from MakeReviewFragment
+        mReview = view.findViewById(R.id.write_review_et);
+        mStar = view.findViewById(R.id.leave_rating_rb);
+
+        // Saving user input into database collection: reviews under restaurants collection.
+        mSendReviewBtn = view.findViewById(R.id.send_review_btn);
+        mSendReviewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                float star = mStar.getNumStars(); // TODO: Fix the number so it does not set as 5 in database
+                String review = mReview.getText().toString();
+
+                Map<String, Object> reviewMap = new HashMap<>();
+                reviewMap.put("user_name", user.getEmail());
+                reviewMap.put("restaurant_star_rating", star);
+                reviewMap.put("user_review", review);
+
+                restaurantDocumentRef.collection("reviews").document().set(reviewMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getActivity(), "Review successfully added", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        String error = e.getMessage();
+                        Toast.makeText(getActivity(), "Error: " + error, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 /*        Query query = restaurantRef.orderBy("ratingStar", Query.Direction.ASCENDING);
 
         FirestoreRecyclerOptions<ReviewInfo> options = new FirestoreRecyclerOptions.Builder<ReviewInfo>()
