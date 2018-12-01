c4c2b75467acfd6486a922cfd0ed3999c0802d96
Ibrahim Mehkri <ibrahimmehkri@gmail.com>
2018-12-01 05:33:40 +0100

    card view clickable

--- app/src/main/java/grupp3/iths/se/wineanddineparalell/SearchAdapter.java
+++ app/src/main/java/grupp3/iths/se/wineanddineparalell/SearchAdapter.java
@@ -1,7 +1,11 @@
 package grupp3.iths.se.wineanddineparalell;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -32,16 +36,45 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.CustomView
 
     @Override
     public void onBindViewHolder(@NonNull CustomViewHolder customViewHolder, int i) {
-        ItemInfo restaurantObj = mRestaurantList.get(i);
+        final ItemInfo restaurantObj = mRestaurantList.get(i);
+
         customViewHolder.mTextView.setText(restaurantObj.getRestaurant_name());
         customViewHolder.mTextPrice.setRating(restaurantObj.getRestaurant_cost_rating());
         customViewHolder.mTextScore.setRating(restaurantObj.getRestaurant_star_rating());
         customViewHolder.mImageView.setImageResource(R.drawable.restaurant);
+
+        customViewHolder.mCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle args = createBundle(restaurantObj);
+                RestaurantFragment restaurantFragment = new RestaurantFragment();
+                restaurantFragment.setArguments(args);
+                MainActivity mainActivity = (MainActivity)mContext;
+                FragmentTransaction transaction = mainActivity.getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.main_frame, restaurantFragment);
+                transaction.commit();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return mRestaurantList.size();
+    }
+
+    private Bundle createBundle(ItemInfo restaurant){
+            Bundle data = new Bundle();
+
+            data.putString("REST_NAME", restaurant.getRestaurant_name());
+            data.putString("REST_ADDRESS", restaurant.getRestaurant_address());
+            data.putString("REST_PHONE", restaurant.getRestaurant_phone_number());
+            data.putString("REST_WEBBSITE", restaurant.getRestaurant_website());
+            data.putFloat("STAR_RATING", restaurant.getRestaurant_star_rating());
+            data.putFloat("PRICE_RATING", restaurant.getRestaurant_cost_rating());
+            data.putBoolean("REST_FOOD", restaurant.isRestaurant_food_type());
+            data.putBoolean("REST_DRINK", restaurant.isRestaurant_drink_type());
+
+            return data;
     }
 
     public class CustomViewHolder extends RecyclerView.ViewHolder{
@@ -50,13 +83,19 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.CustomView
         private RatingBar mTextPrice;
         private RatingBar mTextScore;
         private ImageView mImageView;
+        private CardView mCardView;
 
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
+            initializeViews(itemView);
+        }
+
+        void initializeViews(View view){
             mTextView = itemView.findViewById(R.id.rest_name_tv);
             mTextPrice = itemView.findViewById(R.id.avr_price_rb);
             mTextScore = itemView.findViewById(R.id.avr_score_rb);
             mImageView = itemView.findViewById(R.id.image_view);
+            mCardView = itemView.findViewById(R.id.restaurang_cv);
         }
     }
 }
